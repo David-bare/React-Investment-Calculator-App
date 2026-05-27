e12ae0887b13f9a8b86ad51daaa6f3c90eb4e379
David-bare <davidogunlaja@gmail.com>
2026-05-27 21:04:40 +0100

    Functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
-import Header from './compnents/Header.jsx';
+import { useState } from 'react';
 
-function App() {
+import Header from './compnents/Header.jsx';
+import UserInput from './compnents/UserInput.jsx';
+import Results from './compnents/Results.jsx';
+
+function App() {                           
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const inputIsValid = userInput.duration >= 1;
+
+   function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUsrInput) => {
+      return {
+        ...prevUsrInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+
+
   return (
+    <>
     <Header />
+    <UserInput userInput={userInput} onChange={handleChange} />
+    {!inputIsValid && <p className="center">Please enter a duration greater than zero.</p>}
+    {inputIsValid && <Results input={userInput} />}
+    </>
   )
 }
 
